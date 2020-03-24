800ba1dc3884df9d930bfb64decd5c60faf0079b
Tate Capra <tatecapra@outlook.com>
2020-03-23 19:12:08 -0500

    added to the js file for a button

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// the click function to start the quiz
+document.getElementById("startButton").onclick = function() {
+  runStartButton();
+}
+
+
+function runStartButton(){
+//   document.getElementById("startButton").innerHTML = "Welcome to the Star Wars Quiz";
+console.log("Button Worked ");
+ }
