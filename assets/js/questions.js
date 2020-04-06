6d61562bfff3196adfb3ddbd59535d1063bc8bb1
Tate Capra <tatecapra@outlook.com>
2020-04-06 18:20:02 -0500

    changes to the js files with more outlines being added

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,4 +1,20 @@
 // Star wars questions will go on this page
 
+//each question will need to be displayed then the follow question etc.
+  // question will need to display information then answer choices
+  // once the answer is selected then the next question appears
+  export function questions(q1,q2,q3,q4, q5, q6, q7, q8, q9, q10){
+      
+  }
 
-// Question one Where was Luke Skywalker born 
+
+// Question one Where was Luke Skywalker born
+// Questions two
+// Question three
+// Question four
+// Question five
+// Question six
+// Question seven
+// Question eight
+// Question nine
+// Question ten
